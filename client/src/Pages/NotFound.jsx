0eee55f7734d1ecd0eef0f9f56adf890bba22c7f
ThanuMahee12 <thanumahee440@gmail.com>
2024-10-14 06:49:43 +0530

    Notfound

--- client/src/Pages/NotFound.jsx
+++ client/src/Pages/NotFound.jsx
@@ -6,13 +6,14 @@ function NotFound() {
     console.error(error);
   
     return (
-      <div id="error-page">
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
-      </div>
+        
+    //   <div id="error-page">
+    //     <h1>Oops!</h1>
+    //     <p>Sorry, an unexpected error has occurred.</p>
+    //     <p>
+    //       <i>{error.statusText || error.message}</i>
+    //     </p>
+    //   </div>
     )
 }
 
