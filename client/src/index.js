ae49273e9caec98fb1785a51e8c967ab48a1abe9
ThanuMahee12 <thanumahee440@gmail.com>
2024-10-15 13:33:28 +0530

    fix:packages update and added

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React,{StrictMode} from 'react';
 import ReactDOM from 'react-dom/client'
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
-// import './Styles/index.scss';
+import './Styles/index.scss';
 import App from './App';
 
 
