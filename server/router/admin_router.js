bb5678858ecd4ded1db7b14f01091754d20f9370
ThanuMahee12 <thanumahee440@gmail.com>
2024-10-16 12:13:40 +0530

    feat(project): convert from ES Module to CommonJS - Removed 'type': 'module' from package.json to switch back to CommonJS - Replaced all ES module import statements with CommonJS equire - Replaced all export statements with module.exports - Handled dynamic imports using equire() - Removed file extensions in import paths as required by CommonJS BREAKING CHANGE: Project structure now uses CommonJS instead of ES Modules.

--- server/router/admin_router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const admin_router=express.Router()
-
-admin_router.get("/",(req,res)=>{
-    res.status(200).json([32323,23232,232,323,23,23,2,3,23,2])
-})
-
-
-export default admin_router
