b4d72a2c1c8d2f0b243ce10908adb688d6d87121
ThanuMahee12 <thanumahee440@gmail.com>
2024-10-16 10:10:45 +0530

    feat(pagination):pagination class added

---
+++ server/module/crud_router_module.js
@@ -0,0 +1,14 @@
+class CrudRouter{
+    getall(req,res,next){
+const page=req.query?.page??1
+    }
+    create(req,res,next){
+
+    }
+    delete(req,res,next){
+
+    }
+    getById(req,res,next){
+
+    }
+}
